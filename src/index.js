97c1be0c5ec8df0acd8a5de8e8186e47399a3fff
Aaron Richardson <aaronrichardsonsg@gmail.com>
2024-11-19 17:09:08 +0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,11 @@
 import "./styles.css";
 import { greeting } from "./greeting.js";
 
-// if we want to use images with Webpack we have to import them
-//import odinImage from "./odin.png";
-   
-
-// using image example:
-
-/*const image = document.createElement("img");
-image.src = odinImage;
-   
-document.body.appendChild(image);*/
-
 
 console.log(greeting);
+
+
+//1. make a layout - html/grid
+//2.remember SOLID Principles - seperate appliation logic from DOM related stuff
+//3. create TO-DO object/function - title, description, due-date (date-fns), priority, notes, checklist
+//4. work on projects and to-do functionality 
