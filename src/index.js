fa4449b9dcb5516148f93d2a2dc6ebfd5613b1de
Aaron Richardson <aaronrichardsonsg@gmail.com>
2024-11-26 19:46:27 +0800

    add modal to create task

--- src/index.js
+++ src/index.js
@@ -9,8 +9,23 @@ console.log(greeting);
 //1. make a layout - html/grid
 //2.remember SOLID Principles - seperate appliation logic from DOM related stuff
 //3. create TO-DO object/function - title, description, due-date (date-fns), priority, notes, checklist
-//4. Create modal as a way to populate new task
-//5. work on projects and to-do functionality 
+//4. create modal as a way to populate new task
+//5. connect modal with task object - task to show in todo-container
 
-let createTaskButton = document.getElementById("createTaskButton");
-//modal when button is clicked
+//6. work on projects and to-do functionality 
+
+
+//modal click
+const createTaskButton = document.querySelector("#addTaskButton");
+const modal = document.querySelector("#modal");
+const closeModal = document.querySelector("#close-button");
+
+createTaskButton.addEventListener('click', () => {
+    modal.showModal();
+})
+
+ closeModal.addEventListener('click', () => {
+    modal.close();
+})
+
+//5. connect modal with task object - task to show in todo-container
