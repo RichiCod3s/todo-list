89d5cee392d51d5613a2adb99dcbcfbbc4327980
Aaron Richardson <aaronrichardsonsg@gmail.com>
2024-11-27 21:45:34 +0800

    fix createTodoItem. Form now creates a toDo object

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 
 import "./styles.css";
 import { greeting } from "./greeting.js";
+import { createTodoItem } from "./todo.js";
 
 
 console.log(greeting);
@@ -11,21 +12,37 @@ console.log(greeting);
 //3. create TO-DO object/function - title, description, due-date (date-fns), priority, notes, checklist
 //4. create modal as a way to populate new task
 //5. connect modal with task object - task to show in todo-container
-
-//6. work on projects and to-do functionality 
-
+//6. toDo card to populate the ".todo-container"
+//7. work on projects and to-do functionality 
+//8. work on styling of modal - layout and background
 
 //modal click
 const createTaskButton = document.querySelector("#addTaskButton");
 const modal = document.querySelector("#modal");
-const closeModal = document.querySelector("#close-button");
+const taskSubmitButton = document.querySelector("#task-submit-button");
 
 createTaskButton.addEventListener('click', () => {
     modal.showModal();
 })
 
- closeModal.addEventListener('click', () => {
-    modal.close();
-})
 
 //5. connect modal with task object - task to show in todo-container
+const taskTitle = document.querySelector("#title");
+const taskDescription = document.querySelector("#task-description");
+
+// create todoItem
+taskSubmitButton.addEventListener('click', () => {
+     let title = taskTitle.value;
+     let description = taskDescription.value;
+   let item = createTodoItem(title, description);
+    console.log(item);
+
+    // clear form text fields
+    taskTitle.value = "";
+    taskDescription.value ="";
+})
+
+
+
+//6. toDo card to populate the ".todo-container" (look back at library app)
+
--- src/todo.js
+++ src/todo.js
@@ -1,11 +1,11 @@
 //create TO-DO object/function - title, description, due-date (date-fns), priority, notes, checklist
 import { createElementWithClass } from "./utils"
 
-export function todoItem(title, description){
+export function createTodoItem(title, description){
 let todoTitle = createElementWithClass("h3", "todoTitle", title);
 let todoDescription = createElementWithClass("p", "todoDescription", description);
 
 let todoItem = createElementWithClass("div", "todoItem");
-todoItem.append(todoItem,todoDescription);
+todoItem.append(todoTitle,todoDescription);
 return todoItem;
 }
